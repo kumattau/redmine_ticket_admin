d44bfd915a0f752ff0d40ead332c586fc6cf9c62
kumattau <kumattau@gmail.com>
2010-03-02 22:23:13 +0900

    first commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_ticket_admin do
+  name 'Redmine Ticket Admin plugin'
+  author 'kumattau'
+  description 'This is a ticket admin plugin for Redmine'
+  version '0.0.1'
+end
